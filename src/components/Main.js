f99d4618c347445dab1bf284bcedca4b4eec1e66
unknown <prakashbahadursingh2@gmail.com>
2020-08-27 16:27:59 -0700

    some debugging

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
-import React, { Component, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component} from 'react';
 
 import Grid from "./Grid"
 import Buttons from "./buttons"
